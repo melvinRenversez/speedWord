1b7161f209559b263b4364e0de4f8477492830ba
melvinRenversez <120313822+melvinRenversez@users.noreply.github.com>
2024-01-23 23:11:57 +0100

    maj1

---
+++ script.js
@@ -0,0 +1,70 @@
+content = document.getElementById("content")
+letterText = document.getElementById("letter")
+timer = document.getElementById("timer")
+userAnswer = document.getElementById("answer")
+sendButton = document.getElementById("send")
+startButton = document.getElementById("start")
+listAnswer = document.getElementById("listAnswer")
+
+letterText.innerHTML = "_"
+timer.innerHTML = "0"
+
+var verifacationDone = false
+var currentLetter = ""
+var x
+var Text = ""
+var sizeLetter
+
+alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+allAnswers = []
+
+sizeLetter = alphabet.length
+
+sendButton.addEventListener("click", verification)
+startButton.addEventListener("click", generate)
+
+document.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        verification()
+    }
+})
+
+function printAnswer() {
+    console.log("printAnswer")
+    help = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    for (let i = 0; i < sizeLetter; i++) {
+        console.log(i)
+        allAnswers.forEach(element => {
+            if (element[0] == help[i]) {
+                Text += element[0] + " : " + element[1] + "<br>"
+            }
+        });
+    }
+    listAnswer.innerHTML = Text
+}
+
+function generate(){
+    startButton.classList.remove("active")
+    content.classList.remove("disabled")
+    if(alphabet.length > 0) {
+        x = Math.floor(Math.random() * alphabet.length)
+        letterText.innerHTML = alphabet[x]
+        currentLetter = alphabet[x]
+    }else{
+        letterText.innerHTML = "Finish"
+        printAnswer()
+    }
+}
+
+function verification() {
+    value = userAnswer.value
+    if (value.length >= 3) {
+        if (value.charAt(0).toUpperCase() == currentLetter) {
+            p = [currentLetter, value]
+            allAnswers.push(p)
+            userAnswer.value = ""
+            alphabet.splice(x, 1)
+            generate()
+        }
+    }
+}
